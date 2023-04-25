502da0d20fe8cb6a7893ea50f9e6324ca5d4dd32
Paulo Jerônimo <paulojeronimo@gmail.com>
2023-04-25 18:20:49 -0300

    Updated at 2023-04-25T18:20:48-03:00

--- generate-tmp.sh
+++ generate-tmp.sh
@@ -4,13 +4,17 @@ set -eou pipefail
 tmp_dir=${tmp_dir:-tmp}
 
 cd "$(dirname "$0")"
+! [ "${PWD##*/}" = "$tmp_dir" ] || {
+	echo Current dir is \"$tmp_dir\". Aborting!
+	exit 1
+}
 origin=$(git remote get-url origin)
 new_origin=${origin%.git}
 new_origin=${new_origin%/*}
 new_origin=$new_origin/${tmp_dir}.git
 cd ..
 [ -d "$tmp_dir" ] && {
-  echo "Directory \"$tmp_dir\" already exists. Aborting!"
+  echo Directory \"$tmp_dir\" already exists. Aborting!
   exit 1
 }
 cp -a "$OLDPWD" "$tmp_dir"
